52743db3833ea08553c0ebc7fc6808b4740614f4
Zoi <140983437+3eIlla@users.noreply.github.com>
2023-10-13 05:58:27 -0700

    main.h

---
+++ main.h
@@ -0,0 +1,24 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/*Libraries*/
+#include <stdio.h>    /*For any out in puts */
+
+#include <unistd.h>  /*for display the function write */
+
+#include <stdarg.h>   /*for  */
+
+#include <stdarg.h>   /*for argument after "..."*/
+
+#include <limits.h>   /*for max byet no. */
+
+#include <stdlib.h>
+
+
+
+
+
+
+
+
+#endif
