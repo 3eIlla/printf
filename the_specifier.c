3b5c1c78be4819408666a687359e02e08d91a374
xdSPYCAT <140613943+xdSPYCAT@users.noreply.github.com>
2023-10-14 19:37:21 +0300

    y

--- the_specifier.c
+++ the_specifier.c
@@ -64,16 +64,16 @@ int get_flag(char *s, params_t *params)
  */
 int get_modifier(char *s, params_t *params)
 {
-	int i = 0;
+	int y = 0;
 
 	switch (*s)
 	{
 	case 'h':
-		i = params->h_modifier = 1;
+		y = params->h_modifier = 1;
 		break;
 	case 'l':
-		i = params->l_modifier = 1;
+		y = params->l_modifier = 1;
 		break;
 	}
-	return (i);
+	return (y);
 }
